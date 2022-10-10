31b53423557bb1c5a9aca40ecd06b011e9912736
ehqrwjhntyju <114579833+ehqrwjhntyju@users.noreply.github.com>
2022-10-10 11:19:23 +0000

    dokończono karte pracy 2

---
+++ KartaPracy2.py
@@ -0,0 +1,29 @@
+# # Zad x
+
+# a,b = int(input()), int(input())
+# if a > b:
+#   print("pierwsza jest większa")
+# else:
+#   print("pierwsza jest mniejsza")
+
+# Zad 5
+
+# a,b,c = int(input()), int(input()), int(input())
+# if a < c < b or b < c < a:
+#   print("tak")
+# else:
+#   print("nie")
+
+# a, p = int(input()), int(input())
+# if (a ** p - a) % p == 0:
+#   print("tak, spelnia MTF")
+# else:
+#   print("nie, nie spełnia MTF")
+
+# Zad 7
+# p,k,s = int(input()), int(input()), int(input())
+# if p + 3*s >= k:
+  
+#   print("tak")
+# else:
+#   print("nie")
